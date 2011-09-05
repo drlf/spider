3e3c0010969297335ad8b5d13f046bda7883e533
drlf <drlf@163.com>
2011-09-05 17:48:52 +0800

    完成task manager，task queue简单实现，task xml输出 queue如果队列中没有元素时，会报错。

--- DWCS/grails-app/domain/dwcs/Project.groovy
+++ DWCS/grails-app/domain/dwcs/Project.groovy
@@ -9,9 +9,26 @@ class Project {
 	
 	TaskOwner owner
 	String name
-	Date createTime
+	Date dateCreated
 	int priority
 	TaskType type
+	//检查客户端是否准备好的代码
+	String checkClientPrepare
+	//客户端是准备工作（执行该任务需要下载的插件或资源）
+	String clientPrepare
+	
+	//前提任务
+	String preTask
+	//任务内容
+	String taskContent
+	//提交任务
+	String commitTask
+	
+	//验证task提交的结果
+	String verifyTaskResult
+	
+	
+	
     static constraints = {
     }
 }
--- DWCS/grails-app/domain/dwcs/Task.groovy
+++ DWCS/grails-app/domain/dwcs/Task.groovy
@@ -2,17 +2,18 @@ package dwcs
 
 class Task {
 	//TaskOwner owner
-	Date createTime
+	Date dateCreated
 	Project project
 	//由哪个任务生成的，如果没有父任务则为空
 	Task parentTask
 	//是否被丢弃
 	boolean isDiscard
-	//前提任务
+	//前提任务,如果为空，则采用project的同名项的值
 	String preTask
-	//任务内容，由xml形式组成，
+	//任务内容，由xml形式组成，,如果为空，则采用project的同名项的值
 	String taskContent
-	//
+	//提交任务,如果为空，则采用project的同名项的值
+	String commitTask
 	
     static constraints = {
     }
